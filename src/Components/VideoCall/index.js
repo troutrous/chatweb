0a56573dd9f58ea91a374317a1244522395809a5
vuminhchautroutrous <chouchousisme@gmail.com>
2020-11-28 00:07:40 +0700

    chia phong

--- src/Components/VideoCall/index.js
+++ src/Components/VideoCall/index.js
@@ -62,9 +62,17 @@ const configuration = {
     iceCandidatePoolSize: 10,
 };
 
-const remoteStream = new MediaStream();
+const constraints = {
+    'audio': { 'echoCancellation': true },
+    'video': {
+        'width': 320,
+        'height': 240,
+        cursor: 'always' | 'motion' | 'never',
+        displaySurface: 'application' | 'browser' | 'monitor' | 'window'
+    }
+}
 export default function VideoCall(props) {
-    const [stream, setStream] = useState(null);
+    const [room, setRoom] = useState(null);
     useEffect(() => {
 
     }, []);
@@ -74,100 +82,170 @@ export default function VideoCall(props) {
     }
 
     const createRoom = async () => {
+        const localName = document.querySelector('#nameInput').value;
+        const roomRef = db.collection("rooms").doc();
+        console.log("Room id: " + roomRef.id);
+
+        const localMemberRef = await roomRef.collection('members').doc()
+        await localMemberRef.set(
+            {
+                memberName: localName,
+                memberTimestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            }
+        );
+        let localMember = (await localMemberRef.get()).data();
+
+        roomRef.collection('members').where('memberName', '!=', localName).onSnapshot((doc) => {
+            doc.docChanges().forEach(async (change) => {
+                let memberChange = change.doc.data();
+                if (change.type == 'added') {
+                    let existsPeerConnection = await roomRef.collection('peerConnections')
+                        .where('memberOffer', '==', localName)
+                        .where('memberAnswer', '==', memberChange.memberName)
+                        .get();
+                    if (existsPeerConnection.empty && localMember.memberTimestamp - memberChange.memberTimestamp < 0) {
+                        roomRef.collection('peerConnections').doc().set(
+                            {
+                                memberOffer: localName,
+                                memberAnswer: memberChange.memberName
+                            }
+                        );
+                    }
+                }
+            })
+        })
+    }
+
+    const joinRoom = async () => {
+        const localName = document.querySelector('#nameInput').value;
+        const roomId = document.querySelector('#roomID').value;
+        const roomRef = db.collection('rooms').doc(roomId);
+        const localMemberRef = await roomRef.collection('members').doc()
+        await localMemberRef.set(
+            {
+                memberName: localName,
+                memberTimestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            }
+        );
+        let localMember = (await localMemberRef.get()).data();
+        roomRef.collection('members').where('memberName', '!=', localName).onSnapshot((doc) => {
+            doc.docChanges().forEach(async (change) => {
+                let memberChange = change.doc.data();
+                if (change.type == 'added') {
+                    let existsPeerConnection = await roomRef.collection('peerConnections').where('memberOffer', '==', localName).where('memberAnswer', '==', memberChange.memberName).get();
+                    if (existsPeerConnection.empty && localMember.memberTimestamp - memberChange.memberTimestamp < 0) {
+                        roomRef.collection('peerConnections').doc().set(
+                            {
+                                memberOffer: localName,
+                                memberAnswer: memberChange.memberName
+                            }
+                        );
+                    }
+                }
+            })
+        })
+    }
+
+    const temp = async () => {
 
         // try {
-        const constraints = {
-            'audio': { 'echoCancellation': true },
-            'video': {
-                'width': 320,
-                'height': 240,
-                cursor: 'always' | 'motion' | 'never',
-                displaySurface: 'application' | 'browser' | 'monitor' | 'window'
-            }
-        }
-        const localStream = await navigator.mediaDevices.getUserMedia(constraints);
 
+        const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        const remoteStream = new MediaStream();
 
         document.querySelector('video#localVideo').srcObject = localStream;
         document.querySelector('video#remoteVideo').srcObject = remoteStream;
         // } catch (error) {
         //     console.error('Error opening video camera.', error);
         // }
-        //config cho stun server trả về ip public của client
-        //tạo kết nối peer to peer
-        const peerConnection = new RTCPeerConnection(configuration);
-        // thêm các track âm thanh, hình ảnh của stream vào peer
-        localStream.getTracks().forEach(track => {
-            peerConnection.addTrack(track, localStream);
-        });
-        //tạo mới bản ghi trên cloud -> trả về id phòng
-        const roomRef = db.collection("rooms").doc();
-        console.log("Room id: " + roomRef.id);
 
-        // thêm các icecandidate vào collection người gọi trên cloud
-        peerConnection.onicecandidate = (event) => {
-            if (!event.candidate) {
-                console.log("Got Final Candidate!");
-                return;
-            }
-            console.log('Got candidate: ', event.candidate);
-            roomRef.collection("callerCandidates").add(event.candidate.toJSON());
-        }
+        //tạo mới bản ghi trên cloud -> trả về id phòng
+
+
+
+
+
+        //tạo kết nối peer to peer
+        // const peerConnection = new RTCPeerConnection(configuration);
+        // peerConnection.onconnectionstatechange = (event) => {
+        //     switch (peerConnection.connectionState) {
+        //         case "disconnected":
+        //             remoteStream.getTracks().forEach((track) => {
+        //                 track.stop();
+        //             })
+        //             break;
+        //         case "failed":
+        //             remoteStream.getTracks().forEach((track) => {
+        //                 console.log(remoteStream);
+        //                 remoteStream.removeTrack(track);
+        //             })
+        //             break;
+
+        //         default:
+        //             break;
+        //     }
+        // }
+        // thêm các track âm thanh, hình ảnh của stream vào peer
+        // localStream.getTracks().forEach(track => {
+        //     peerConnection.addTrack(track, localStream);
+        // });
+
+        // // thêm các icecandidate vào collection người gọi trên cloud
+        // peerConnection.onicecandidate = (event) => {
+        //     if (!event.candidate) {
+        //         console.log("Got Final Candidate!");
+        //         return;
+        //     }
+        //     console.log('Got candidate: ', event.candidate);
+        //     roomRef.collection("callerCandidates").add(event.candidate.toJSON());
+        // }
         //tạo offer
 
 
-        const offer = await peerConnection.createOffer();
-        const roomWithOffer = {
-            offer: {
-                type: offer.type,
-                sdp: offer.sdp
-            }
-        }
-        //cập nhật lại room
-        await roomRef.set(roomWithOffer);
-        //đặt localdescription
-        await peerConnection.setLocalDescription(offer);
+        // const offer = await peerConnection.createOffer();
+        // const roomWithOffer = {
+        //     offer: {
+        //         type: offer.type,
+        //         sdp: offer.sdp
+        //     }
+        // }
+        // //cập nhật lại room
+        // await roomRef.set(roomWithOffer);
+        // //đặt localdescription
+        // await peerConnection.setLocalDescription(offer);
 
 
-        //view các track khách
-        peerConnection.ontrack = (event) => {
-            event.streams[0].getTracks().forEach(track => {
-                remoteStream.addTrack(track);
-            });
-        }
+        // //view các track khách
+        // peerConnection.ontrack = (event) => {
+        //     event.streams[0].getTracks().forEach(track => {
+        //         remoteStream.addTrack(track);
+        //     });
+        // }
 
-        //lắng nghe thay đổi thông tin callee
-        roomRef.collection('calleeCandidates').onSnapshot(doc => {
-            doc.docChanges().forEach(async change => {
-                if (change.type === 'added') {
-                    console.log("add icecandidate");
-                    await peerConnection.addIceCandidate(change.doc.data());
-                }
-            });
-        });
+        // //lắng nghe thay đổi thông tin callee
+        // roomRef.collection('calleeCandidates').onSnapshot(doc => {
+        //     doc.docChanges().forEach(async change => {
+        //         if (change.type === 'added') {
+        //             console.log("add icecandidate");
+        //             await peerConnection.addIceCandidate(change.doc.data());
+        //         }
+        //     });
+        // });
 
-        //lắng nghe answer
-        roomRef.onSnapshot(async doc => {
-            const data = doc.data();
-            if (data && data.answer) {
-                await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-            }
-        });
-        
+        // //lắng nghe answer
+        // roomRef.onSnapshot(async doc => {
+        //     const data = doc.data();
+        //     if (data && data.answer) {
+        //         await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+        //     }
+        // });
+
 
     };
 
-    const joinRoom = async () => {
-        const constraints = {
-            'audio': { 'echoCancellation': true },
-            'video': {
-                'width': 640,
-                'height': 480,
-                cursor: 'always' | 'motion' | 'never',
-                displaySurface: 'application' | 'browser' | 'monitor' | 'window'
-            }
-        }
+    const temp2 = async () => {
         const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        const remoteStream = new MediaStream();
         document.querySelector('video#localVideo').srcObject = localStream;
         document.querySelector('video#remoteVideo').srcObject = remoteStream;
         //lấy giá trị phòng
@@ -179,6 +257,24 @@ export default function VideoCall(props) {
 
         if (roomSnapshot.exists) {
             const peerConnection = new RTCPeerConnection(configuration);
+            peerConnection.onconnectionstatechange = (event) => {
+                switch (peerConnection.connectionState) {
+                    case "disconnected":
+                        remoteStream.getTracks().forEach((track) => {
+                            track.stop();
+                        })
+                        break;
+                    case "failed":
+                        remoteStream.getTracks().forEach((track) => {
+                            remoteStream.removeTrack(track);
+                        })
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
             localStream.getTracks().forEach(track => {
                 peerConnection.addTrack(track, localStream);
             });
@@ -228,6 +324,7 @@ export default function VideoCall(props) {
     };
     return (
         <div className="videoCallContainer">
+            <Form.Control type="text" placeholder="Name" id="nameInput" />
             <Button variant="outline-primary" type="button" className="btn" onClick={createRoom}>
                 Create
             </Button>
